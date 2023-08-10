5b2ca005295f1bbb30809b91468222b82e80aef5
TheLittle_Yang <2570984321@qq.com>
2023-08-10 14:04:46 +0800

    感受了一下恐怖的回调地狱

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import { createOpenAPI, createWebsocket } from "qq-guild-bot";
 import axios from "axios";
 import { config } from "./config.js";
+import Query from "mcquery/lib/index.js";
+
+const HOST = process.env.MC_SERVER || "mc3.roselle.vip";
+const PORT = process.env.MC_PORT || 29236;
 
 const botConfig = {
   appID: config.appID, // 申请机器人时获取到的机器人 BotAppID
@@ -14,27 +18,61 @@ const client = createOpenAPI(botConfig);
 // 创建 websocket 连接
 const ws = createWebsocket(botConfig);
 
+// 马的，这 mcquery 的 sb 回调函数，搞心态。暂时不知道更好的实现办法
+let query, last_channel_id, last_msg_id;
+
 ws.on("GUILD_MESSAGES", (data) => {
   if (data.msg.content.includes("/list")) {
-    axios
-      .get("https://mcapi.us/server/status?ip=play.toiletmc.net")
-      .then((respone) => {
-        const onlinePlayers = respone.data.players.sample.map(
-          (item) => item.name
-        );
-        client.messageApi
-          .postMessage(data.msg.channel_id, {
-            msg_id: data.msg.id,
-            content:
-              `在线玩家（${respone.data.players.now}/${respone.data.players.max}）：` +
-              onlinePlayers.join(", "),
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+    query = new Query(HOST, PORT);
+    query
+      .connect()
+      .then(() => {
+        console.log("Asking for full_stat");
+        last_channel_id = data.msg.channel_id;
+        last_msg_id = data.msg.id;
+        query.full_stat(fullStatBack);
       })
-      .catch(function (error) {
-        console.log(error);
+      .catch((err) => {
+        console.error("error connecting", err);
       });
   }
 });
+
+function fullStatBack(err, stat) {
+  if (err) {
+    console.error(err);
+  }
+
+  const respone = {
+    online: stat.numplayers,
+    maxplayers: stat.maxplayers,
+    players: stat.player_,
+  };
+
+  sendMessage(
+    last_channel_id,
+    last_msg_id,
+    `在线玩家（${respone.online}/${respone.maxplayers}）：` +
+      respone.players.join("，")
+  );
+
+  shouldWeClose();
+}
+
+function shouldWeClose() {
+  // have we got all answers
+  if (query.outstandingRequests === 0) {
+    query.close();
+  }
+}
+
+function sendMessage(channel_id, msg_id, message) {
+  client.messageApi
+    .postMessage(channel_id, {
+      msg_id: msg_id,
+      content: message,
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
